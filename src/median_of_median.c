543a123fd4911f9984fc91d00a149ce9691013e5
中西　貴大 <taka@nakanishitakashidainoMacBook-ea.local>
2019-07-02 01:30:18 +0900

    aaa

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -3,7 +3,7 @@
 // 素数
 #define N 2999
 
-int A[N];
+int A[N],B[N];
 
 // *p と *q の値を入れ替える関数
 void swap(int *p, int *q){
@@ -15,11 +15,8 @@ void swap(int *p, int *q){
 
 int quick_select(int A[], int n, int k){
   int i, j, pivot;
-
-// 真ん中の要素をピボットとする
-  pivot = A[n/2];
-  A[n/2] = A[0];
-  A[0] = pivot;
+  if(n <= 5){
+        pivot = A[0];
   for(i = j = 1; i < n; i++){
     if(A[i] <= pivot){
       swap(A+i, A+j);
@@ -27,10 +24,50 @@ int quick_select(int A[], int n, int k){
     }
   }
 
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
-}
+      if(j == k+1) return pivot;
+      else if(j < k+1) return quick_select(A+j, n-j, k-j);
+      else return quick_select(A+1, j-1, k);
+  }
+      
+      
+                   else {
+        int x;
+        x = 0;
+                                  for(i = 0;i < (n+9)/5;i++){
+                                  if(n-x >= 5){
+                               B[i] = quick_select(A+x, 5, 2);
+                                  x=x+5;
+                           }
+            else B[i] = quick_select(A+x, n-x,(n-x)/2);
+        }
+        pivot = quick_select(B, (n+4)/5, (n+4)/10);
+        int a,b;
+        for(a = 0;a < n; a++){
+            if(A[a] == pivot){
+                b = a;
+                a = n-1;
+            }
+        }
+           A[b] = A[0];
+           A[0] = pivot;
+           for(i = j = 1; i < n; i++){
+            if(A[i] <= pivot){
+                swap(A+i, A+j);
+                j++;
+            }
+        }
+        
+       
+                       
+                       if(j == k+1) return pivot;
+        else if(j < k+1) return quick_select(A+j, n-j, k-j);
+        else return quick_select(A+1, j-1, k);
+        
+                   } }
+
+    
+    
+ 
 
 
 int main(){
