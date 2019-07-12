6117ea9bb8d9b442981df306dfb8c6d4a5782947
中西　貴大 <taka@nakanishitakashidainoMacBook-ea.local>
2019-07-13 00:01:00 +0900

    teisei

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -18,18 +18,41 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int i, j, pivot;
+    
+    pivot = A[n/2];
+    A[n/2] = A[0];
+    A[0] = pivot;
+    for(i = j = 1; i < n; i++){
+        if(A[i] <= pivot){
+            swap(A+i, A+j);
+            j++;
+            
+            
+        }
+    }
+    
+    if(n > 0){
+        
+        swap(A,A+j-1);
+        
+        quick_sort(A, j-1);
+        
+        quick_sort(A+j, n-j);
+        
+    }
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 17; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 17; //原始元
+    for(i=2;i<N;i++){
+        A[i] = (long long int) A[i-1] * A[1] % N;
+    }
+    
+    quick_sort(A, N);
+    for(i=0;i<N;i++){
+        if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+    }
 }
